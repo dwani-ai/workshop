64cfa496e56901860c7766f4c251c67c0cfea72a
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2025-07-20 12:53:34 +0200

    session 4 (#11)
    * add-
    
    * fix-reposen
    
    * resume

--- intro/llm_chat.py
+++ intro/llm_chat.py
@@ -1,11 +1,17 @@
 import dwani
 import os
 dwani.api_key = 'your_api_key'
-dwani.api_base = 'your_dwani_url'
+dwani.api_base = 'dwani api url'
 def main():
-    print("hello")
-    resp = dwani.Chat.create(prompt="Hello!", src_lang="english", tgt_lang="kannada")
+    resp = dwani.Chat.create(prompt="Hello!", src_lang="english", tgt_lang="kannada", model="gemma3")
+
+
     print(resp)
+
+    resp = dwani.Chat.direct(prompt="Hello! I am sachin")
+
+    print(resp)
+
     
 if __name__ == "__main__":
     main()
--- intro/text_to_speech.py
+++ intro/text_to_speech.py
@@ -4,7 +4,7 @@ dwani.api_key = 'your_api_key'
 dwani.api_base = 'your_dwani_url'
 def main():
     print("hello")
-    response = dwani.Audio.speech(input="ಕರ್ನಾಟಕ ದ ರಾಜಧಾನಿ ಯಾವುದು", response_format="mp3")
+    response = dwani.Audio.speech(input="ಕರ್ನಾಟಕ ದ ರಾಜಧಾನಿ ಯಾವುದು", response_format="mp3", language="kannada")
     with open("output.mp3", "wb") as f:
         f.write(response)
 if __name__ == "__main__":
